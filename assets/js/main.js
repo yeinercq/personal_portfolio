4ad93a57cf464e1e76ef2bec328dafa9efbb5dfd
Yeiner Carvajal <ycarvajalq@gmail.com>
2022-03-08 09:57:20 -0500

    Cambios para Fixed positioned scrollable sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('#sidebar').mCustomScrollbar({
+      theme: "minimal"
+    });
+
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
